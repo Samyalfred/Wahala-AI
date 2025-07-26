59eeac25ec291a9574921b4ed1d71246594a9377
Samyalfred <ekanemsalek@gmail.com>
2025-07-26 10:36:37 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("videoForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Wahala AI is still warming up. Real video generator coming soon!");
+});
